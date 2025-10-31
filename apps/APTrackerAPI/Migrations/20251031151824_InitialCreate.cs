12d786b9baf766afd54c3f766c35d75fb121eb7e
Mischa Heeb <mischa.heeb@hotmail.com>
2025-10-31 17:31:21 +0100

    chore(api): Added Database migrations to API

---
+++ apps/APTrackerAPI/Migrations/20251031151824_InitialCreate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace APTrackerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Attractions",
+                columns: table => new
+                {
+                    AttractionId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AttractionName = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    TrackWaitingTime = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attractions", x => x.AttractionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WaitingTime",
+                columns: table => new
+                {
+                    WaitingTimeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AttractionId = table.Column<int>(type: "integer", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    WaitingTimeInMinutes = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WaitingTime", x => x.WaitingTimeId);
+                    table.ForeignKey(
+                        name: "FK_WaitingTime_Attractions_AttractionId",
+                        column: x => x.AttractionId,
+                        principalTable: "Attractions",
+                        principalColumn: "AttractionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WaitingTime_AttractionId",
+                table: "WaitingTime",
+                column: "AttractionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WaitingTime_Timestamp",
+                table: "WaitingTime",
+                column: "Timestamp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WaitingTime");
+
+            migrationBuilder.DropTable(
+                name: "Attractions");
+        }
+    }
+}
--- apps/APTrackerAPI/Program.cs
+++ apps/APTrackerAPI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using APTrackerAPI.Data;
 using APTrackerAPI.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,28 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Initialize Database
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var logger = services.GetRequiredService<ILogger<Program>>();
+
+    try
+    {
+        logger.LogInformation("Initializing database");
+        var context = services.GetRequiredService<APTrackerDbContext>();
+
+        context.Database.Migrate();
+
+        logger.LogInformation("Database successfully initialized!");
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, "Database could not be initialized!");
+        throw;
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
