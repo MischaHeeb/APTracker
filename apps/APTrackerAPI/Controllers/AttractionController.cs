d37cc3d901992fdae816e9bbf4165e4c9b615f74
Mischa Heeb <mischa.heeb@hotmail.com>
2025-11-14 23:01:16 +0100

    chore(api): Added documentation to Methods and Class inside of AttractionController.cs

--- apps/APTrackerAPI/Controllers/AttractionController.cs
+++ apps/APTrackerAPI/Controllers/AttractionController.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace APTrackerAPI.Controllers
 {
+    /// <summary>
+    /// Controller for managing attractions.
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class AttractionController : ControllerBase
@@ -18,12 +21,24 @@ namespace APTrackerAPI.Controllers
             _dbContext = dbContext;
         }
 
+        /// <summary>
+        /// Retrieves all attractions.
+        /// </summary>
+        /// <returns>A list of all attractions.</returns>
+        /// <response code="200">Returns the list of attractions</response>
         [HttpGet("GetAttraction")]
         public async Task<ActionResult<IEnumerable<Attraction>>> GetAttraction()
         {
             return await _dbContext.Attractions.ToListAsync();
         }
 
+        /// <summary>
+        /// Retrieves a specific attraction by a given ID.
+        /// </summary>
+        /// <param name="id">The ID of the attraction to retrieve.</param>
+        /// <returns>The attraction specified by the ID.</returns>
+        /// <response code="200">Returns the requested attraction</response>
+        /// <response code="404">If the attraction was not found</response>
         [HttpGet("GetAttraction/{id}")]
         public async Task<ActionResult<Attraction>> GetAttraction(int id)
         {
